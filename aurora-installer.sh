81a1ab2ef4f41ff7bfd83fe19da3a00f9e2dc398
Aru Sahni <arusahni@gmail.com>
2013-11-09 16:49:49 -0500

    Update aurora-installer.sh

--- aurora-installer.sh
+++ aurora-installer.sh
@@ -2,7 +2,7 @@
 
 # Copyright © 2013 Aru Sahni <arusahni@gmail.com>
 # This work is free. It comes without any warranty, to the extent
-# permitted by applicable law.You can redistribute it and/or modify it
+# permitted by applicable law. You can redistribute it and/or modify it
 # under the terms of the Do What The Fuck You Want To Public License,
 # Version 2, as published by Sam Hocevar.
 # See the COPYING file for more details.
